ca7f07279aa8ac318b32c0715eb9f4a9795a759b
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2019-12-26 18:32:15 -0500

    handler test

--- handler_test.go
+++ handler_test.go
@@ -1,7 +1,8 @@
 package main
 
-/*
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -42,7 +43,6 @@ func TestGetUUID(t *testing.T) {
 	}
 
 	testExpectJSONContentType(t, rr)
-
 }
 
 func testExpectJSONContentType(t *testing.T, rr *httptest.ResponseRecorder) {
@@ -55,6 +55,13 @@ func testExpect200(t *testing.T, rr *httptest.ResponseRecorder) {
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
+	}
+}
+
+func testExpect409(t *testing.T, rr *httptest.ResponseRecorder) {
+	if status := rr.Code; status != http.StatusConflict {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusConflict)
 	}
 }
 func TestGetInfo(t *testing.T) {
@@ -77,6 +84,7 @@ func TestGetInfo(t *testing.T) {
 }
 
 func TestPutDatabase(t *testing.T) {
+	kdb, _ = NewKDB()
 	req, _ := http.NewRequest("PUT", "/testdb", nil)
 	rr := httptest.NewRecorder()
 	handler := NewRouter()
@@ -92,6 +100,176 @@ func TestPutDatabase(t *testing.T) {
 	testExpectJSONContentType(t, rr)
 }
 
+func TestHandlerPutDocument(t *testing.T) {
+	body := bytes.NewBufferString("{}")
+	req, _ := http.NewRequest("POST", "/testdb", body)
+	rr := httptest.NewRecorder()
+	handler := NewRouter()
+	handler.ServeHTTP(rr, req)
+
+	testExpect200(t, rr)
+	doc, _ := ParseDocument(rr.Body.Bytes())
+	if doc.Version != 1 || doc.ID == "" {
+		t.Errorf(`expected to have ok, got %s`, rr.Body.String())
+	}
+
+	testExpectJSONContentType(t, rr)
+}
+
+func TestHandlerPutDocument1(t *testing.T) {
+	handler := NewRouter()
+
+	body := bytes.NewBufferString(`{"_id":1}`)
+	req, _ := http.NewRequest("POST", "/testdb", body)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	testExpect200(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	doc, _ := ParseDocument(rr.Body.Bytes())
+	if doc.Version != 1 || doc.ID != "1" {
+		t.Errorf(`expected to have ok, got %s`, rr.Body.String())
+	}
+
+	body = bytes.NewBufferString(`{"_id":1, "_version":1}`)
+	req, _ = http.NewRequest("POST", "/testdb", body)
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	testExpectJSONContentType(t, rr)
+	testExpect200(t, rr)
+
+	doc, _ = ParseDocument(rr.Body.Bytes())
+	if doc.Version != 2 || doc.ID != "1" {
+		t.Errorf(`expected to have ok, got %s`, rr.Body.String())
+	}
+
+	body = bytes.NewBufferString(`{"_id":1, "_version":1}`)
+	req, _ = http.NewRequest("POST", "/testdb", body)
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	testExpect409(t, rr)
+}
+
+func TestHandlerDeleteDocument(t *testing.T) {
+	handler := NewRouter()
+
+	req, _ := http.NewRequest("DELETE", "/testdb/1?version=2", nil)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	testExpect200(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	doc, _ := ParseDocument(rr.Body.Bytes())
+
+	if doc.ID != "1" || doc.Version != 3 || doc.Deleted != true {
+		t.Errorf(`expected to have ok, got %s`, rr.Body.String())
+	}
+}
+
+func TestHandlerPutDeletedDocument(t *testing.T) {
+	handler := NewRouter()
+
+	body := bytes.NewBufferString(`{"_id":1, "_version":2}`)
+	req, _ := http.NewRequest("POST", "/testdb", body)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	testExpect409(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	body = bytes.NewBufferString(`{"_id":1}`)
+	req, _ = http.NewRequest("POST", "/testdb", body)
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	testExpect200(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	doc, _ := ParseDocument(rr.Body.Bytes())
+
+	if doc.ID != "1" || doc.Version != 4 || doc.Deleted != false {
+		t.Errorf(`expected to have ok, got %s`, rr.Body.String())
+	}
+}
+
+func TestHandlerBulkDocuments(t *testing.T) {
+	body := bytes.NewBufferString(`{"_docs":[{"_id":3},{"_id":4}]}`)
+	req, _ := http.NewRequest("POST", "/testdb/_bulk_docs", body)
+	rr := httptest.NewRecorder()
+	handler := NewRouter()
+	handler.ServeHTTP(rr, req)
+	expected := `[{"_id":"3","_version":1},{"_id":"4","_version":1}]`
+
+	testExpect200(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	if expected != rr.Body.String() {
+		t.Errorf(`expected to have %s, got %s`, expected, rr.Body.String())
+	}
+}
+
+func TestHandlerBulkGetDocuments(t *testing.T) {
+	body := bytes.NewBufferString(`{"_docs":[{"_id":3},{"_id":4}]}`)
+	req, _ := http.NewRequest("POST", "/testdb/_bulk_gets", body)
+	rr := httptest.NewRecorder()
+	handler := NewRouter()
+	handler.ServeHTTP(rr, req)
+	expected := `[{"_id":"3","_version":1},{"_id":"4","_version":1}]`
+
+	testExpect200(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	if expected != rr.Body.String() {
+		t.Errorf(`expected to have %s, got %s`, expected, rr.Body.String())
+	}
+}
+
+type testChanges struct {
+	Results []testChange `json:"results"`
+}
+
+type testChange struct {
+	ID      string `json:"id"`
+	Version int    `json:"version"`
+	Seq     string `json:"seq"`
+}
+
+func TestHandlerGetChanges(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/testdb/_changes", nil)
+	rr := httptest.NewRecorder()
+	handler := NewRouter()
+	handler.ServeHTTP(rr, req)
+
+	testExpect200(t, rr)
+	testExpectJSONContentType(t, rr)
+
+	a := testChanges{}
+	json.Unmarshal(rr.Body.Bytes(), &a)
+
+	a0 := a.Results[0]
+	if a0.ID != "4" || a0.Version != 1 {
+		t.Errorf(`failed`)
+	}
+
+	a1 := a.Results[1]
+	if a1.ID != "3" || a1.Version != 1 {
+		t.Errorf(`failed`)
+	}
+
+	a2 := a.Results[2]
+	if a2.ID != "1" || a2.Version != 4 {
+		t.Errorf(`failed`)
+	}
+
+	a4 := a.Results[4]
+	if a4.ID != "_design/_views" || a4.Version != 1 {
+		t.Errorf(`failed`)
+	}
+}
 func TestDeleteDatabase(t *testing.T) {
 	req, _ := http.NewRequest("DELETE", "/testdb", nil)
 	rr := httptest.NewRecorder()
@@ -107,4 +285,3 @@ func TestDeleteDatabase(t *testing.T) {
 
 	testExpectJSONContentType(t, rr)
 }
-*/
