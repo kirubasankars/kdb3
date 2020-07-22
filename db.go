e586fab5efa0875a58ed31ac92a6daa0d838926c
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2020-07-22 17:21:51 -0400

    added steps to live vacuuming

--- db.go
+++ db.go
@@ -285,6 +285,19 @@ func (db *DefaultDatabase) GetStat() *DatabaseStat {
 // Vacuum vacuum
 func (db *DefaultDatabase) Vacuum() error {
 	writer := <-db.writer
+	/*
+			1. Copy data (with max update seq) to new data file
+			2. Close Writer
+			3. Copy remaining data (with min (max from step 1) and max new update seq) to new data file
+			4. Close all readers
+			5. Close viewManager and views
+			6. Update localDB with new data file name
+			7. Create writer and open it
+			8. Create Readers and open it all
+			9. Push writer and readers to its corresponding channels
+		   10. Open viewManager and views
+		   11. Delete old data file
+	*/
 	defer func() {
 		db.writer <- writer
 	}()
